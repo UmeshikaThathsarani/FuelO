7e30ca98c6d0c9d575812660eb7b7cad4032c060
Umeshika Thathsarani Mahaliyadda <utmahaliyadda@gmail.com>
2023-04-18 10:58:48 +0530

    added bew fields to the form

--- fuel-o-gui/src/app/pages/prediction/Prediction.js
+++ fuel-o-gui/src/app/pages/prediction/Prediction.js
@@ -11,11 +11,13 @@ class Prediction extends Component {
     this.state = {
       year : '',
       make : '',
+      _class : '',
+      drive : '',
       transmission : '',
       engineCylinders: '',
       turbocharger: '',
-      fuelType: '',
-      mpg: ''
+      supercharger: '',
+      fuelType: ''
     }
 
   }
@@ -33,7 +35,7 @@ class Prediction extends Component {
   handleInputChange1 = (event) =>{
     event.preventDefault()
     this.setState({
-        year : Number(event.target.value)
+        year : event.target.value
     })
   }
 
@@ -47,42 +49,56 @@ class Prediction extends Component {
   handleInputChange3 = (event) =>{
     event.preventDefault()
     this.setState({
-        transmission : event.target.value
+        _class : event.target.value
     })
   }
 
   handleInputChange4 = (event) =>{
     event.preventDefault()
     this.setState({
-        engineCylinders: Number(event.target.value)
+        drive : event.target.value
     })
   }
 
   handleInputChange5 = (event) =>{
     event.preventDefault()
     this.setState({
-        turbocharger: event.target.value
+        transmission : event.target.value
     })
   }
-
 
   handleInputChange6 = (event) =>{
     event.preventDefault()
     this.setState({
-        fuelType: event.target.value
+        engineCylinders: Number(event.target.value)
     })
   }
 
   handleInputChange7 = (event) =>{
     event.preventDefault()
     this.setState({
-      mpg : Number(event.target.value)
+        turbocharger: event.target.value
+    })
+  }
+
+  handleInputChange8 = (event) =>{
+    event.preventDefault()
+    this.setState({
+        supercharger: event.target.value
+    })
+  }
+
+
+  handleInputChange9 = (event) =>{
+    event.preventDefault()
+    this.setState({
+        fuelType: event.target.value
     })
   }
 
   render(){
 
-    const {year, make, transmission, engineCylinders, turbocharger, fuelType, mpg} = this.state
+    const {year, make, _class, drive, transmission, engineCylinders, turbocharger, supercharger, fuelType} = this.state
 
     return(
 
@@ -116,49 +132,67 @@ class Prediction extends Component {
                     </div>
 
                     <div className="input-divs" >
-                      <h3>3. Transmission
+                      <h3>3. Vehicle Class
                           <span>
                               <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="text" required
-                              id="transmission" name="transmission" onChange={this.handleInputChange3}
+                              id="_class" name="_class" onChange={this.handleInputChange3}
+                              value={_class}/>
+                          </span>
+                        </h3>
+                    </div>
+
+                    <div className="input-divs" >
+                      <h3>4. Drive
+                          <span>
+                              <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="text" required
+                              id="drive" name="drive" onChange={this.handleInputChange4}
+                              value={drive}/>
+                          </span>
+                        </h3>
+                    </div>
+
+                    <div className="input-divs" >
+                      <h3>5. Transmission
+                          <span>
+                              <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="text" required
+                              id="transmission" name="transmission" onChange={this.handleInputChange5}
                               value={transmission}/>
                           </span>
                         </h3>
                     </div>
 
                     <div className="input-divs" >
-                      <h3>4. Number of Engine Cylinders
+                      <h3>6. Number of Engine Cylinders
                         <span>
                           <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="number" required
-                          id="engineCylinders" name="engineCylinders" onChange={this.handleInputChange4}
+                          id="engineCylinders" name="engineCylinders" onChange={this.handleInputChange6}
                           step="1" min='2' max='16' value={engineCylinders} />
                         </span>
                       </h3>
                     </div>
 
-                    <h3>5. Is Turbocharger In Use 
+                    <h3>7. Is Turbocharger In Use 
                     <div className="radio-divs">
-                      <Radio.Group onChange={this.handleInputChange5} value={turbocharger}>
+                      <Radio.Group onChange={this.handleInputChange7} value={turbocharger}>
                           <Radio id="turbocharger_yes" value={1} name="turbocharger">Yes</Radio>
                           <Radio id="turbocharger_no" value={0} name="turbocharger">No</Radio>
                       </Radio.Group>
                     </div></h3>
 
-                    <div className="input-divs" >
-                      <h3>6. Fuel Type
-                        <span>
-                          <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="text" required
-                          id="fuelType" name="fuelType" onChange={this.handleInputChange6}
-                          value={fuelType}/>
-                        </span>
-                      </h3>
-                    </div>
+                    <h3>8. Is Supercharger In Use 
+                    <div className="radio-divs">
+                      <Radio.Group onChange={this.handleInputChange8} value={supercharger}>
+                          <Radio id="supercharger_yes" value={1} name="supercharger">Yes</Radio>
+                          <Radio id="supercharger_no" value={0} name="supercharger">No</Radio>
+                      </Radio.Group>
+                    </div></h3>
 
                     <div className="input-divs" >
-                      <h3>7. MPG
+                      <h3>9. Fuel Type
                         <span>
-                          <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="number"
-                          id="mpg" name="mpg" onChange={this.handleInputChange7}
-                          step="0.01" min='0' value={mpg}/>
+                          <input style={{marginLeft:"2%", float:"right", width:"200px"}} type="text" required
+                          id="fuelType" name="fuelType" onChange={this.handleInputChange9}
+                          value={fuelType}/>
                         </span>
                       </h3>
                     </div>
